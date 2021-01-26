800ee5592001c32f219ca191f87b2505f55a5e91
Ahmaz Athias <ahmaz.athias@bison.howard.edu>
2021-01-25 21:09:46 -0500

    HW 1

--- org.howard.edu.lsp.assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ org.howard.edu.lsp.assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,6 +1,8 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
+	
+	//Ahmaz Athias
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!"); //printing out to user 
